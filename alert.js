d32f0ad57eb6ad5a198951c93fc0570b99b421e2
Brian <otto72886@m365.bfwbb-lernen.de>
2025-03-14 18:46:22 +0100

    Add "use strict"; and explain it

--- alert.js
+++ alert.js
@@ -1,1 +1,15 @@
+"use strict";
+
+// the "use strict"; at the top enables strict mode for all of the code in the file
+// ONLY POSSIBLE IF IT'S THE FIRST STATEMENT
+
+/* 
+"use strict";
+function example() {
+    return something;
+}
+
+is also usable, when strict mode should only be applied to chosen function/s
+*/
+
 alert("Hello World!");
