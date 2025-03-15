ff54035386a553c0cd21bb973bf09b939f68768b
Brian <otto72886@m365.bfwbb-lernen.de>
2025-03-15 19:27:20 +0100

    Add some dom-manipulation

---
+++ dom-manipulation.js
@@ -0,0 +1,25 @@
+const container = document.querySelector("#container");
+
+const redText = document.createElement("p");
+redText.style.color = "red";
+redText.textContent = "Hey I'm red!";
+container.appendChild(redText);
+
+const blueH3 = document.createElement("h3");
+blueH3.style.color = "blue";
+blueH3.textContent = "I'm a blue h3!";
+container.appendChild(blueH3);
+
+const div = document.createElement("div");
+div.style.border = "1px solid black";
+div.style.backgroundColor = "pink";
+
+const h1 = document.createElement("h1");
+h1.textContent = "I'm in a div";
+div.appendChild(h1);
+
+const p = document.createElement("p");
+p.textContent = "ME TOO!";
+div.appendChild(p);
+
+container.appendChild(div);
